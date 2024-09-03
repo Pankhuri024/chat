d91b9bee5768cba306309d689e817e0df0831770
deepraj41352 <137053649+deepraj41352@users.noreply.github.com>
2024-09-03 08:01:01 +0000

    create sent-text route

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    response_data = {'message': 'Hello, this is data from Flask!'}
-    return jsonify(response_data)
+@app.route('/api/send-text', methods=['POST'])
+def receive_text():
+    data = request.get_json()
+    received_text = data.get('text', '')  # Get 'text' field from JSON data
+    return jsonify({'received_text': received_text, 'message': 'Text received successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
